701fbca31788b85de4cd8c0b9a72330359d4c8e3
Jeff <jkrejcik@ymail.com>
2019-12-10 23:31:21 -0600

    Completed HTML parser

---
+++ KrejcikWebScraper/src/HtmlParser.java
@@ -0,0 +1,77 @@
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class HtmlParser {
+	
+	/**
+	 * Parse Course ID from HTML
+	 */
+	public static String parseId(String line) {
+		line = line.substring(line.indexOf("<span>")+6);
+		line = line.substring(0, line.indexOf("</span>"));
+		return line;
+	}
+	
+	/**
+	 * Parse Course Name from HTML
+	 */
+	public static String parseName(String line) {
+		line = line.substring(line.indexOf("</span>")+8);
+		line = line.replace("</a>", "");
+		return line;
+	}
+	
+	/**
+	 * Parse Course Description from HTML
+	 */
+	public static String parseDescription(String line) {
+		line = line.replace("<p>", "");
+		line = line.replace("</p>", "");
+		return line;
+	}
+	
+	
+	/**
+	 Reads course data from a web URL.
+	 @param fname, target text file path	 
+	 */
+	public static ArrayList<Course> parseCourseData(String html) {
+		try {
+			URL link = new URL(html);
+			Scanner usc = new Scanner(link.openStream());
+			String line, id, name, description, credits;
+			Course tempCourse;
+			ArrayList<Course> result = new ArrayList<Course>();
+			
+			while (usc.hasNextLine()) {
+				try {
+					if(usc.nextLine().contains("\"courseList\"")) {
+						usc.nextLine();
+						line = usc.nextLine();
+						id = parseId(line);
+						name = parseName(line);
+						
+						usc.nextLine();
+						line = usc.nextLine();
+						description = parseDescription(line);
+						
+						usc.nextLine();
+						line = usc.nextLine();
+						credits = line;
+						
+						tempCourse = new Course(id, name, description, credits);
+						result.add(tempCourse);
+					}
+				} catch (Exception ex) {}
+			}
+			usc.close();
+			return result;
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+}
